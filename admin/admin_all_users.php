b595ae3541057bbaf7607209813c39ba173db3a7
marklanderyou <marklanderyou@macbookpro.local>
2017-03-26 17:09:41 -0400

    update

--- admin/admin_all_users.php
+++ admin/admin_all_users.php
@@ -2,9 +2,12 @@
   $pageTitle = "Admin";
   
   require_once('phpscripts/init.php');
-
+  
   confirm_logged_in();
  ?>
+
+ <?php 
+  ?>
  <!doctype html>
 <html class="no-js" lang="en" dir="ltr">
   <head>
@@ -66,7 +69,7 @@
       </li>
       <li><a data-dropdown="drop3" aria-controls="drop3" aria-expanded="false">Users</a>
     <ul id="drop3" class="f-dropdown" data-dropdown-content aria-hidden="true" tabindex="-1">
-      <li><a href="admin_all_users.php">All Users</a></li>
+      <li><a href="#">All Users</a></li>
       <li><a href="#">Add New User</a></li>
       <li><a href="#">Edit User</a></li>
     </ul>
@@ -77,7 +80,7 @@
   <section>
     <div class="row">
       <div class="medium -10 large-10 columns">
-        <h1>Welcome to Heart2Heart Admin Page</h1>
+        <h1>All Users</h1>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam impedit ut fugit nihil adipisci, ipsam quibusdam incidunt voluptas, nostrum consequatur quo praesentium officiis, aspernatur provident. Soluta sapiente dignissimos provident doloribus.</p>
         <p>Laboriosam vel aperiam, consequatur reprehenderit ad exercitationem quibusdam sequi alias ipsum adipisci. Debitis, ad iste assumenda consequuntur dicta aspernatur repellendus hic architecto unde corporis voluptate. Vitae aliquid, corrupti numquam laboriosam.</p>
         <p>Ut soluta maiores eos ullam amet alias cum, non pariatur accusantium quasi tempora, dicta? Assumenda ullam cum dolorem quibusdam eaque natus numquam, voluptas officia eum minus ad pariatur ab quae.</p>
